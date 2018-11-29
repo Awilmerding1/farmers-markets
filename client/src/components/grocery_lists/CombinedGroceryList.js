c0272d522719ebb21fdc97c7cdc6cf5523ea759a
Annabel Wilmerding <annabelwilmerding@gmail.com>
2018-11-29 16:15:05 -0500

    filtering fixed

--- client/src/components/grocery_lists/CombinedGroceryList.js
+++ client/src/components/grocery_lists/CombinedGroceryList.js
@@ -8,8 +8,8 @@ class CombinedGroceryList extends Component  {
     this.state = {
       markets: this.marketsAndItems(),
       clicked: false,
-      sortGroceries: false,
-      sortDay: false
+      sortGroceries: 0,
+      sortDay: 0
     }
   }
 
@@ -21,17 +21,24 @@ class CombinedGroceryList extends Component  {
   return marketItems
   }
 
-  clicked = () => {
+  clicked = (event) => {
     this.setState({clicked: !this.state.clicked})
   }
 
-  sortGroceries = () => {
-    this.setState({sortGroceries: !this.state.sortGroceries})
+  sortGroceries = (event) => {
+    if (this.state.sortGroceries <= this.state.sortDay) {
+    this.setState({sortGroceries: event.timeStamp, sortDay: 0})
+  } else {
+      this.setState({sortGroceries: 0})
+  }
   }
 
-  sortDay = () => {
-    this.setState({sortDay: !this.state.sortDay})
-
+  sortDay = (event) => {
+    if (this.state.sortDay <= this.state.sortGroceries) {
+      this.setState({sortDay: event.timeStamp, sortGroceries: 0})
+    } else {
+        this.setState({sortDay: 0})
+    }
   }
 
   getDays = () => {
@@ -39,16 +46,23 @@ class CombinedGroceryList extends Component  {
     return z.filter((v, i, a) => a.indexOf(v) === i)
   }
 
+  returnToList = () => {
+    if (this.state.sortDay === 0 && this.state.sortGroceries === 0 ){
+
+    }
+  }
+
 render () {
+
 
 return (
   <div>
     <div className="showMarketNames" onClick={this.clicked}>Show/Hide Market Names</div>
-    <div className="sortGroceries" onClick={this.sortGroceries}>{!this.state.sortGroceries ? "Sort By Market" : "Show One List"}</div>
+    <div className="sortGroceries" onClick={this.sortGroceries}>{this.state.sortGroceries === 0 ? "Sort By Market" : "Show One List"}</div>
     <div className="sortDay" onClick={this.sortDay}>Sort By Day</div>
-    <div>{!this.state.sortGroceries && !this.state.sortDay && this.state.markets.map(market => market.items.map(item => <li key={item.id}>{item.text} {!this.state.clicked ? " " :  "- " + market.facilityname}</li>))}</div>
-    <div>{this.state.sortGroceries && !this.state.sortDay && this.state.markets.map(market => <p key={cuidFn()} className="marketSort">{market.facilityname} {market.items.map(item => <li className="sortedByMarket" key={cuidFn()}>{item.text}</li>)}</p>)}</div>
-    <div>{this.state.sortDay && this.getDays().map(day => <p className="marketSort" key={cuidFn()}>{day.charAt(0).toUpperCase() + day.slice(1)} {this.props.markets.filter(market => market[day]).map(market => <li className="sortedByMarket" key={cuidFn()}>{market.items.map(item => item.text)} {!this.state.clicked ? " " :  "- " + market.facilityname}</li>)}</p>)}</div>
+    <div>{this.state.sortGroceries === 0 && this.state.sortDay === 0 && this.state.markets.map(market => market.items.map(item => <li key={item.id}>{item.text} {!this.state.clicked ? " " :  "- " + market.facilityname}</li>))}</div>
+    <div>{this.state.sortGroceries > this.state.sortDay && this.state.markets.map(market => <p key={cuidFn()} className="marketSort">{market.facilityname} {market.items.map(item => <li className="sortedByMarket" key={cuidFn()}>{item.text}</li>)}</p>)}</div>
+    <div>{this.state.sortDay > this.state.sortGroceries && this.getDays().map(day => <p className="marketSort" key={cuidFn()}>{day.charAt(0).toUpperCase() + day.slice(1)} {this.props.markets.filter(market => market[day]).map(market => <li className="sortedByMarket" key={cuidFn()}>{market.items.map(item => item.text)} {!this.state.clicked ? " " :  "- " + market.facilityname}</li>)}</p>)}</div>
   </div>
 
 )
