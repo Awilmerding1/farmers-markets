20eec72da630e09cb2fad0ca2ce3b594a370b7b0
Annabel Wilmerding <annabelwilmerding@gmail.com>
2018-11-29 13:58:20 -0500

    fixed days

--- client/src/components/farmers_markets/FarmersMarket.js
+++ client/src/components/farmers_markets/FarmersMarket.js
@@ -5,12 +5,15 @@ export const cuidFn = cuid;
 const FarmersMarket = ({farmersMarket}) => {
 
   const days = Object.keys(farmersMarket).filter(key => key === "monday" || key === "tuesday" || key === "wednesday" || key === "thursday" || key === "friday" || key === "saturday" || key === "sunday")
+  const dayArray = []
+  const dayValues = days.forEach(day => {if (farmersMarket[day] !== null && farmersMarket[day] !== "") {dayArray.push(day)}})
+
 
   return (
   <div className= "marketSearchandList">
     <div></div>
     <li  className="marketListItem">
-      {farmersMarket.facilityname} - {farmersMarket.address}, {farmersMarket.borough} {farmersMarket.zipcode} {days.map(day => <p key={cuidFn()} className="daysOpen">{day.charAt(0).toUpperCase() + day.slice(1) + ": " + farmersMarket[day]}</p>)}<br/>
+      {farmersMarket.facilityname} - {farmersMarket.address}, {farmersMarket.borough} {farmersMarket.zipcode} {dayArray.map(day => <p key={cuidFn()} className="daysOpen">{day.charAt(0).toUpperCase() + day.slice(1) + ": " + farmersMarket[day]}</p>)}<br/>
       <GroceryListsContainer farmersMarket={farmersMarket} />
     </li><br/><br/>
   </div>
