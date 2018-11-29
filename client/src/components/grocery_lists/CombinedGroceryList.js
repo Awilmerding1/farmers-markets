56cca4a73499917662af14616804e1b0fbc1b28a
Annabel Wilmerding <annabelwilmerding@gmail.com>
2018-11-29 16:17:34 -0500

    toggle show one list

--- client/src/components/grocery_lists/CombinedGroceryList.js
+++ client/src/components/grocery_lists/CombinedGroceryList.js
@@ -59,7 +59,7 @@ return (
   <div>
     <div className="showMarketNames" onClick={this.clicked}>Show/Hide Market Names</div>
     <div className="sortGroceries" onClick={this.sortGroceries}>{this.state.sortGroceries === 0 ? "Sort By Market" : "Show One List"}</div>
-    <div className="sortDay" onClick={this.sortDay}>Sort By Day</div>
+    <div className="sortDay" onClick={this.sortDay}>{this.state.sortDay === 0 ? "Sort By Day" : "Show One List"}</div>
     <div>{this.state.sortGroceries === 0 && this.state.sortDay === 0 && this.state.markets.map(market => market.items.map(item => <li key={item.id}>{item.text} {!this.state.clicked ? " " :  "- " + market.facilityname}</li>))}</div>
     <div>{this.state.sortGroceries > this.state.sortDay && this.state.markets.map(market => <p key={cuidFn()} className="marketSort">{market.facilityname} {market.items.map(item => <li className="sortedByMarket" key={cuidFn()}>{item.text}</li>)}</p>)}</div>
     <div>{this.state.sortDay > this.state.sortGroceries && this.getDays().map(day => <p className="marketSort" key={cuidFn()}>{day.charAt(0).toUpperCase() + day.slice(1)} {this.props.markets.filter(market => market[day]).map(market => <li className="sortedByMarket" key={cuidFn()}>{market.items.map(item => item.text)} {!this.state.clicked ? " " :  "- " + market.facilityname}</li>)}</p>)}</div>
