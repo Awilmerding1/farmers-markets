3521e6c03ee1ca5dd32df497e48977d8ba0935ba
Annabel Wilmerding <annabelwilmerding@gmail.com>
2019-03-15 10:55:21 -0400

    removed counter from project review

--- client/src/components/farmers_markets/FarmersMarket.js
+++ client/src/components/farmers_markets/FarmersMarket.js
@@ -2,21 +2,8 @@ import cuid from 'cuid';
 import React, { Component } from 'react';
 import GroceryListsContainer from '../../containers/GroceryListsContainer'
 export const cuidFn = cuid;
-class FarmersMarket extends Component {
+const FarmersMarket = ({farmersMarket}) => {
 
-  constructor(props) {
-    super(props)
-    this.state = {
-      counter: 0
-    }
-  }
-
-  clicked = () => {
-    this.setState({counter: this.state.counter + 1})
-  }
-
-  render() {
-  const {farmersMarket} = this.props
   const days = Object.keys(farmersMarket).filter(key => key === "monday" || key === "tuesday" || key === "wednesday" || key === "thursday" || key === "friday" || key === "saturday" || key === "sunday")
   const dayArray = []
   const dayValues = days.forEach(day => {if (farmersMarket[day] !== null && farmersMarket[day] !== "") {dayArray.push(day)}})
@@ -24,14 +11,13 @@ class FarmersMarket extends Component {
 
   return (
   <div className= "marketSearchandList">
-    <button onClick={this.clicked}>Counter: {this.state.counter}</button>
+  <div></div>
     <li  className="marketListItem">
       {farmersMarket.facilityname} - {farmersMarket.address}, {farmersMarket.borough} {farmersMarket.zipcode} {dayArray.map(day => <p key={cuidFn()} className="daysOpen">{day.charAt(0).toUpperCase() + day.slice(1) + ": " + farmersMarket[day]}</p>)}<br/>
       <GroceryListsContainer farmersMarket={farmersMarket} />
     </li><br/><br/>
   </div>
 )
-}
 }
 
 export default FarmersMarket;
